17043653223eba48a86d58dc9113832c45d208ce
IsuruFerna <isurumadushan.ferna@gmail.com>
2024-02-05 11:10:58 +0100

    dependancies added

---
+++ src/main/java/epicode/capstoneepicode/CapstoneEpicodeApplication.java
@@ -0,0 +1,13 @@
+package epicode.capstoneepicode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapstoneEpicodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CapstoneEpicodeApplication.class, args);
+	}
+
+}
