0e2e8abcc01541412892675efb0f96d71cfd271a
IsuruFerna <isurumadushan.ferna@gmail.com>
2024-02-16 15:13:10 +0100

    fix(user): hides credentialsNonExpired in user GET response

--- src/main/java/epicode/capstoneepicode/entities/user/User.java
+++ src/main/java/epicode/capstoneepicode/entities/user/User.java
@@ -20,7 +20,7 @@ import java.util.UUID;
 @NoArgsConstructor
 @Entity
 @Table(name = "users")
-@JsonIgnoreProperties({"password", "authorities", "accountNonExpired", "enabled", "accountNonLocked", "credentialNonLocked", "postList"})
+@JsonIgnoreProperties({"password", "authorities", "accountNonExpired", "enabled", "accountNonLocked", "credentialNonLocked", "credentialsNonExpired", "postList"})
 public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
